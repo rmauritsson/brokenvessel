fcb87b2175b434a3b876ed91a656b64575dfb855
Leo <ronald.mauritsson@gmail.com>
2019-12-29 05:15:48 +0300

    Added devise gem, added migrations to add extra fields to the sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controller => {registrations: 'registrations'} 
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
+  root 'pages#community'
+
+  resources :users
 end
