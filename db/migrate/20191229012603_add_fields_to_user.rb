ea40c2c5d0dfffd23c0da79f1ae230aea244ee59
Kanyesigye Edgar <kedgar751@gmail.com>
2020-03-02 21:50:30 +0300

    prefer RSpec to the builtin tests for testing

--- db/migrate/20191229012603_add_fields_to_user.rb
+++ db/migrate/20191229012603_add_fields_to_user.rb
@@ -1,7 +1,11 @@
 class AddFieldsToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :username, :string
-    add_column :users, :fullname, :string
-    add_column :users, :contact, :string
+    change_table :users, bulk: true do |t|
+      t.string :fullname, null: false
+      t.string :username, null: false
+      t.string :contact, null: false
+      t.boolean :admin, default: false
+      t.boolean :listener, default: false
+    end
   end
 end
