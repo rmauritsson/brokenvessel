3f99913cd0485afc1a8733fb9779d4033f31dc6e
Kanyesigye Edgar <kedgar751@gmail.com>
2020-03-04 17:04:07 +0300

    add user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:fullname) }
+  it { is_expected.to validate_presence_of(:contact) }
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_presence_of(:email) }
 end
